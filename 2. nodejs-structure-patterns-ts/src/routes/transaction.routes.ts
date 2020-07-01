d4bf210ee149ff2f63caf06611d3d53b5d3d90cb
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-07-01 00:51:23 -0300

    feat: Add nodejs-structure-patters-ts project.

---
+++ 2. nodejs-structure-patterns-ts/src/routes/transaction.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+const createTransactionService = new CreateTransactionService(
+  transactionsRepository,
+);
+
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transaction = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    const result = {
+      transactions: transaction,
+      balance,
+    };
+    return response.status(200).json(result);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (request, response) => {
+  try {
+    const { title, value, type } = request.body;
+
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.status(201).json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
---
+++ 2. nodejs-structure-patterns-ts/src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface ResquestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: ResquestDTO): Transaction {
+    const getBalance = this.transactionsRepository.getBalance();
+
+    if (getBalance.total < value && type === 'outcome') {
+      throw Error(
+        'Should not be able to create outcome transaction without a valid balance',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
